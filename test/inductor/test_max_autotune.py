5eb5132fa9b59a391c0d66e7f4e6be0ac5531d32
Glen Cao <glen.cao@amd.com>
2026-07-27 00:31:16 -0700

    [ROCm][Inductor] Use file-backed generic compile test

--- test/inductor/test_max_autotune.py
+++ test/inductor/test_max_autotune.py
@@ -6059,8 +6059,6 @@ class TestTDMConfigDenseAndGeneric(TestCase):
         if not self._tdm_capable_triton():
             self.skipTest("Triton without gfx1250 TDM backend support")
 
-        import subprocess
-        import sys
         import textwrap
 
         child = textwrap.dedent(
@@ -6117,12 +6115,7 @@ class TestTDMConfigDenseAndGeneric(TestCase):
             print("<<<SEP>>>".join(outputs))
             """
         )
-        proc = subprocess.run(
-            [sys.executable, "-c", child],
-            capture_output=True,
-            text=True,
-            timeout=600,
-        )
+        proc = run_triton_code_in_subprocess(child)
         if proc.returncode != 0:
             self.fail(proc.stderr.strip())
         parts = proc.stdout.split("<<<SEP>>>")
